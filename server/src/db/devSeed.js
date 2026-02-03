02cc1f859f53bf3e24e19fd53978bb96ef8f4fd9
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2026-02-02 22:59:31 -0800

    updated devSeed with additional test users and hashed passwords

--- server/src/db/devSeed.js
+++ server/src/db/devSeed.js
@@ -1,12 +1,29 @@
 import { user } from "../models/index.js";
+import { hashPassword } from "../utils/hashPassword.js";
 
 export async function seedDevUser() {
+  const hashedPassword = await hashPassword("password123");
+
   await user.create({
-    email: "test@example.com",
+    email: "testuser1@example.com",
     first_name: "Test",
-    last_name: "User",
-    password_hash: "password123",
+    last_name: "User1",
+    password_hash: hashedPassword,
   });
 
-  console.log("DB reset and seeded with Test User");
+  await user.create({
+    email: "testuser2@example.com",
+    first_name: "Test",
+    last_name: "User2",
+    password_hash: hashedPassword,
+  });
+
+  await user.create({
+    email: "testuser3@example.com",
+    first_name: "Test",
+    last_name: "User3",
+    password_hash: hashedPassword,
+  });
+
+  console.log("DB reset and seeded with Test Users");
 }
