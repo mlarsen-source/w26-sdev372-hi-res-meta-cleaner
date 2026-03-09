e60036c58c10d764a284415f9662f9bb586d13d1
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2026-03-08 20:51:53 -0700

    test for logging in a user from home page

--- front-end/cypress/e2e/home.cy.ts
+++ front-end/cypress/e2e/home.cy.ts
@@ -6,8 +6,9 @@ describe("Home Page", () => {
     it("displays the header on load", () => {
         cy.get("h1").should("be.visible");
         cy.get("h1").should("have.text", "Hi-Res Meta Cleaner");
-    })
+    });
 
+    //After running this test, restart backend since a user can only register once
     it("registering users", () => {
         cy.get("nav").should("be.visible");
         cy.get("[data-testid='login-link']").should("be.visible");
@@ -51,5 +52,26 @@ describe("Home Page", () => {
         cy.get("[class='user-greeting']").should("have.text", "Miku");
     });
 
+    it("log in user", () => {
+        cy.get("[data-testid='login-link']").should("be.visible");
+        cy.get("[data-testid='login-link']").should("have.text", "Login");
+        cy.get("a[href='/login']").click()
+
+        cy.url().should("include", "/login");
+        cy.get("h2").should("be.visible", "Login");
+
+        cy.get('[data-testid="email-input"]').should("be.visible");
+        cy.get('[data-testid="email-input"]').type("HatsuneMiku39@vmail.com");
+        cy.get('[data-testid="password-input"]').should("be.visible");
+        cy.get('[data-testid="password-input"]').type("leek86cecb");
+
+        cy.get('[class="submit-button"]').should("be.visible");
+        cy.get('[class="submit-button"]').should("have.text", "Login");
+        cy.get("[class='submit-button']").click()
+
+        cy.url().should("include", "/");
+        cy.get("[class='user-greeting']").should("be.visible")
+        cy.get("[class='user-greeting']").should("have.text", "Miku");
+    })
 
 });
